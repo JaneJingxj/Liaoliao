75f145a023e2987d20e192e746d89584e17b30e1
Totoro12 <1245881179@qq.com>
2015-04-28 10:31:31 +0800

    李丹121111

--- app/src/main/java/com/xxj/liaoliao/adapter/ChatMessageAadapter.java
+++ app/src/main/java/com/xxj/liaoliao/adapter/ChatMessageAadapter.java
@@ -23,6 +23,8 @@ import java.util.List;
 public class ChatMessageAadapter extends BaseAdapter{
     /**
      * 当前聊天信息列表     我改变了它
+     *
+     *
      */
     private List<ChatMessage> messages;
 
